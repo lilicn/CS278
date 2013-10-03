2bd43fe25b27902ad039470533b6541fce088da7
Li Li <skyw932@gmail.com>
2013-10-03 12:29:21 -0500

    dependency-injection-spring

---
+++ dependency-injection-spring/src/org/cs27x/di/test/DataStoreImplTest.java
@@ -0,0 +1,14 @@
+package org.cs27x.di.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DataStoreImplTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ dependency-injection-spring/src/org/cs27x/di/test/PhotoStorageImplTest.java
@@ -0,0 +1,14 @@
+package org.cs27x.di.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PhotoStorageImplTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
